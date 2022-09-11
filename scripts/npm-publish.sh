8979e29719f93c8355c8f27bb58609540c92443f
Jón Levy <nonni@nonni.cc>
2022-09-11 17:12:09 +0000

    ci: fix publish script, improved lint cmds

--- scripts/npm-publish.sh
+++ scripts/npm-publish.sh
@@ -2,25 +2,42 @@
 
 set -euox pipefail
 
+
+function create_rc() {
+  local version
+  version=$1
+  # shellcheck disable=SC2086
+  ./build/index.js calendar.rc -r $version -f yy.mm.dd
+}
+
+function create_cal() {
+  local version
+  version=$1
+  # shellcheck disable=SC2086
+  ./build/index.js calendar -r $version -f yy.mm.dd
+}
+
 # shellcheck disable=SC2002
 current_version="$(cat package.json \
   | grep version \
   | head -1 \
   | awk -F: '{ print $2 }' \
   | sed 's/[",]//g' | xargs)"
-# current_date=$(date +'%-y.%-m.%-d')
-# echo "current date: $current_date"
 
-# if [ "$current_version" == "$current_date" ]; then
-#   echo "same calver date: $current_version == $current_date, creating an rc version instead .."
-#   # shellcheck disable=SC2086
-#   new_version=$(./build/index.js calendar.rc -r $current_version -f yy.mm.dd)
-# else
-#   # shellcheck disable=SC2086
-#   new_version=$(./build/index.js calendar -r $current_version -f yy.mm.dd)
-# fi
+current_date=$(date +'%-y.%-m.%-d')
 
-new_version=$(./build/index.js calendar.beta -r $current_version -f yy.mm.dd)
+echo "current date: $current_date"
+
+if [ "$current_version" = "$current_date" ] || [[ "$current_version" = *"rc"* ]]; then
+  echo "$current_version" = "$current_date"
+  echo "$current_version includes rc"
+  echo "creating an rc version ..."
+  new_version=$(create_rc "$current_version")
+else
+  new_version=$(create_cal "$current_version")
+fi
+
+# new_version=$(./build/index.js calendar.beta -r $current_version -f yy.mm.dd)
 
 # shellcheck disable=SC2086
 echo "new version: ${new_version}"
