6a618caad3b2435beec2e58ff06e01cf359f1261
lommi <lodmfjord@users.noreply.github.com>
2022-09-13 00:38:33 +0000

    chore: smaller slug (#20)
    * feat: new smaller and worse slugify library
    
    * fix: lowercase
    
    * fix: prettier
    
    * fix: linter
    
    Co-authored-by: jonorn <jonorn@reon.is>

---
+++ src/lib/slugify.ts
@@ -0,0 +1,10 @@
+// Small and dumb slugify. It's not a full slugify implementation, but it's enough for our needs.
+export const slugify = (msg: string): string => {
+  return msg
+    .normalize('NFD')
+    .replace(/[^\x20-\x7E]/g, '')
+    .replace(/[\s/\\]/g, '-')
+    .replace(/[^a-zA-Z0-9-]/g, '')
+    .replace(/[-]{2,}/g, '-')
+    .toLowerCase();
+};
